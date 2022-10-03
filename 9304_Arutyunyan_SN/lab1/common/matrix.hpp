e05ee4298ec4f41b5e1d3a5262345d62a93ce047
Samvel Arutyunyan <sttiemath@yandex-team.ru>
2022-10-03 21:24:15 +0300

    remove lab1 and finish

--- 9304_Arutyunyan_SN/lab1/common/matrix.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <utility>
-
-using Row = std::vector<int>;
-using Matrix = std::vector<Row>;
-using MatrixPair = std::pair<Matrix, Matrix>;
-using MatrixValueType = Row::value_type;
