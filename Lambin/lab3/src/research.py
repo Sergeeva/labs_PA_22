edc9d3929a18f205b56c078e29e8c8f2f8c03a9d
Alexey Lambin <lambin1483alex@gmail.com>
2022-11-01 03:22:07 +0300

    [lab3] Add research script

---
+++ Lambin/lab3/src/research.py
@@ -0,0 +1,34 @@
+import sys
+import subprocess
+import time
+import matplotlib.pyplot as plt
+
+
+def run(program_name_1, program_name_2):
+    amount = [i for i in range(10, 501, 5)]
+    seconds_1 = []
+    seconds_2 = []
+    for matrix_amount in amount:
+        start = time.time()
+        with subprocess.Popen([f'{program_name_1} {matrix_amount} /dev/null'], shell=True):
+            pass
+        seconds_1.append(time.time() - start)
+        start = time.time()
+        with subprocess.Popen([f'{program_name_2} {matrix_amount} /dev/null'], shell=True):
+            pass
+        seconds_2.append(time.time() - start)
+    plt.plot(amount, seconds_1, 'ro-',
+             amount, seconds_2, 'bo-')
+    plt.xlabel('Matrix amount')
+    plt.ylabel('Lead time, sec')
+    plt.savefig('size_change.png')
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage:\n\tpython3 research.py <file_name_1> <file_name_2>")
+    run(sys.argv[1], sys.argv[2])
+
+
+if __name__ == '__main__':
+    main()
