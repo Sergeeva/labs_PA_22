feaeb16419890d13c3d4e632b6d57f8072f6a2f1
N3xTr1Me <destriker17@gmail.com>
2022-12-26 14:16:43 +0300

    Final build

--- 0381_Sokolov_Dmitry/lab4/src/main.cpp
+++ 0381_Sokolov_Dmitry/lab4/src/main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
-//#include "./matrix/MatrixHandler.h"
+#include <chrono>
+
 #include "./matrix/multiplication/Strassen.h"
-#include "./utilities/session_timer.h"
+
+
+using namespace std::chrono;
+
 
 int main() {
 
-    Strassen st = Strassen();
+    Matrix A = MatrixHandler::create(Config::R, Config::C, 322);
+    Matrix B = MatrixHandler::create(Config::R, Config::C, 228);
 
-    Matrix A = MatrixHandler::create(Config::R, Config::C, 228);
-    Matrix B = MatrixHandler::create(Config::R, Config::C, 322);
+    auto start = steady_clock::now();
+    Matrix C = MatrixHandler::parallel_mult(A, B, Config::execution_threads);
+    Logger::info("Simple parallelization finished within: " +
+                  std::to_string(duration_cast<microseconds>(steady_clock::now() - start).count()));
 
-    Matrix C = Matrix(A.get_rows(), B.get_columns());
+    Logger::info("-----------------------------------------------------------------\n\n", true);
 
-    C = st.parallel_mult(A, B, Config::threads);
+    Logger::dump(Config::log_path);
 
-    Matrix correct = A * B;
-    Matrix parallel = MatrixHandler::parallel_mult(A, B, Config::threads);
+    Logger::info("Strassen algorithm started\n", true);
 
-    std::cout << (C == correct && C == parallel) << std::endl;
+    start = steady_clock::now();
+
+    Matrix D = Strassen::parallel_mult(A, B, Config::execution_threads);
+
+    Logger::info("Strassen finished within: " +
+                 std::to_string(duration_cast<microseconds>(steady_clock::now() - start).count()));
+
+    Logger::info("Validation: " + std::to_string(C == D) + "\n");
+
+    Logger::dump(Config::log_path);
 
     return 0;
 
