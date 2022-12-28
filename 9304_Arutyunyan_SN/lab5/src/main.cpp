094defb1121797e3c1c50620db86bcbf74ec172a
Samvel Arutyunyan <sttiemath@gmail.com>
2022-12-29 02:32:53 +0300

    init

--- 9304_Arutyunyan_SN/lab5/src/main.cpp
+++ 9304_Arutyunyan_SN/lab5/src/main.cpp
@@ -1,23 +1,20 @@
 #include "mandelbrot_cpu.hpp"
-#include "mandelbrot.hpp"
 
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <string>
 #include <fstream>
 
-#define WIDTH 1920
-#define HEIGHT 1080
-#define ITERATIONS 256
-#define COEFFICIENT 4
-#define RESULT_FILE_PATH "mandelbrot.ppm"
-#define RESULT_CPU_FILE_PATH "mandelbrot_cpu.ppm"
+constexpr std::size_t WIDTH = 1920;
+constexpr std::size_t HEIGHT = 1080;
+const std::string FILE_PATH = "mandelbrot_cpu.ppm";
 
-void Save(const cl_uint *result, const std::string &name, int height, int width) {
-    std::ofstream file(name, std::ios::binary);
+void Save(const std::vector<unsigned int>& result, std::size_t height, std::size_t width) {
+    std::ofstream file(FILE_PATH, std::ios::binary);
     file << "P6\n" << width << " " << height << "\n255\n";
     for (int y = 0; y < height; ++y) {
-        const unsigned int *line = result + width * y;
+        const unsigned int *line = result.data() + width * y;
         for (int x = 0; x < width; ++x) {
             file.write((const char *) (line + x), 3);
         }
@@ -27,16 +24,9 @@ void Save(const cl_uint *result, const std::string &name, int height, int width)
 int main() {
     {
         auto startTime = clock();
-        std::vector<cl_uint> result = mandelbrot(COEFFICIENT, HEIGHT, WIDTH, ITERATIONS);
+        std::vector<unsigned int> result = mandelbrotCpu(5, HEIGHT, WIDTH, 256);
         std::cout << "Time taken: " << (clock() - startTime) / CLOCKS_PER_SEC << std::endl;
-        Save(result.data(), RESULT_FILE_PATH, HEIGHT, WIDTH);
-    }
-
-    {
-        auto startTime = clock();
-        std::vector<unsigned int> result = mandelbrotCpu(COEFFICIENT, HEIGHT, WIDTH, ITERATIONS);
-        std::cout << "Time taken: " << (clock() - startTime) / CLOCKS_PER_SEC << std::endl;
-        Save(result.data(), RESULT_CPU_FILE_PATH, HEIGHT, WIDTH);
+        Save(result, HEIGHT, WIDTH);
     }
 
     return 0;
